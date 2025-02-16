ec34d6de999746fb028c238ffea36112ffdfe72f
Francesco Tizzani <francesco@tizzani.org>
2025-02-16 15:35:53 +0800

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 def calculate_indices(num_phases, material_prices, reuse_factors, material_requirements, cpis, years):
     fdci_values_no_inflation = []
     fdci_values_with_inflation = []
